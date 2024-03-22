496f85fe0d8053f6a5b7bf251bed9d6253185cfd
fozil <fozi@l.com>
2024-03-22 14:08:14 +0300

    master

---
+++ scr.js
@@ -0,0 +1,17 @@
+var son1 = +prompt('видите чесло')
+var son2 = +prompt('видите чесло')
+var son3 = +prompt('видите чесло')
+
+if(son1 > son2){
+    alert('в центре ' + son2)
+}
+else if(son1 < son2){
+    alert('в центре ' + son2)
+}
+else if(son3 < son2){
+    alert('в центре ' + son2)
+}
+else if(son3 > son2){
+    alert('в центре ' + son2)
+}
+
